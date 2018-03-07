60d70871e4b30b99df065073ca80a1d593686a1d
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-07 15:45:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
@@ -25,3 +26,20 @@ function doWhileLoop(array) {
   } while (array.length > 0 && maybeTrue())
 return array
 }
+=======
+var loops = ['I am a strange loop', 'I am ${i} strange loops.']
+function forLoop(array) {
+  array.push('I am a strange loop.', 'I am ${i} strange loops.')
+ } for (let i = 0; i < 25; i++)
+  if (i = 1) {
+    console.log("I am 1 strange loop.")
+     } else {
+        console.log("I am ${i} strange loops.")
+}
+
+
+
+
+
+
+>>>>>>> ad3f401ad0f68146457ec4b60cd7e9f72ed5ead6
